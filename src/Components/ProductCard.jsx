5938914b8ca6f53e770fb2a38889278d7823de2c
sankalp patel <sankalppatel38@gmail.com>
2026-06-16 17:34:32 +0530

    Implemented_product_listing

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+function ProductCard({ product }) {
+  
+  const { title, image, price, category } = product
 
-function ProductCard() {
   return (
-    <div>ProductCard</div>
+    <div className="product-card">
+      {/* Image */}
+
+      <div className="product-image-wrapper">
+        <img src={image} alt={title} className="product-image" />
+      </div>
+
+      {/* Content */}
+
+      <div className="product-content">
+        <h3 className="product-title">{title}</h3>
+
+        <h3>
+          <span style={{ color: 'red' }}>Category</span>: {category}
+        </h3>
+
+        <p className="product-price">₹ {Math.floor(price)}</p>
+
+        <button className="add-cart-btn">Add To Cart</button>
+      </div>
+    </div>
   )
 }
 
